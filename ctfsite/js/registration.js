4285f685a703aaf887e99bdd4eda4e2686fdcbff
Strikeskids <github@strikeskids.com>
2015-04-04 10:22:53 -0400

    Registration shows and hides correctly

--- ctfsite/js/registration.js
+++ ctfsite/js/registration.js
@@ -1,6 +1,8 @@
 jQuery(function($) {
     var form = $('#registration-form')
     var fields = form.find('input, select')
+    var backgroundField = form.find('#background')
+    var teacherButton = form.find('#team-teacher-button')
     var newTeacher = form.find('#create-new-teacher')
     var newTeam = form.find('#create-new-team')
 
@@ -8,15 +10,32 @@ jQuery(function($) {
     var teamButtons = form.find('.team-style-button')
 
     function selectTeamOption(option) {
-        var selected = $("#"+option+"-information")
+        var nextOption = $("#"+option+"-information")
+        var prevOption = $(options).filter(':visible')
         newTeam.val((option === 'team-create')+'')
         newTeacher.val((option === 'team-teacher')+'')
-        options.find('input').prop('required', false)
-        selected.find('input').prop('required', true)
-        options.slideUp({queue: false})
-        selected.css('height','')
-        selected.slideDown({queue: false})
+        prevOption.find('input')
+            .prop('required', false)
+        prevOption.slideUp({queue: false}).promise()
+            .done(function() {
+                nextOption.find('input')
+                    .prop('required', true)
+                nextOption
+                    .css('height', '')
+                    .slideDown({queue: false})
+            })
     }
+
+    function showTeamButtons() {
+        var teacher = backgroundField.find('option:selected').data('teacher')
+        if (teacher) {
+            teacherButton.show()
+        } else {
+            teacherButton.hide()
+        }
+    }
+
+    backgroundField.on('change', showTeamButtons)
 
     teamButtons.on('click', function(e) {
         selectTeamOption($(e.currentTarget).val())
@@ -30,8 +49,10 @@ jQuery(function($) {
 
         tjctf.apiQuery('POST', '/api/user/create', data)
             .done(function(data) {
-                options.filter(':visible').find('button[type="submit"]')
+                $(options).filter(':visible').find('button[type="submit"]')
                     .apiNotify(data, data['create-new-teacher'] === 'true' ? '/classroom' : '/team')
             })
     })
+
+    showTeamButtons()
 })
